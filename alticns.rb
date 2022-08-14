9077cd7bbef732134798415394c7f3bf181a4401
Fus1onDev <luke_s1220@yahoo.co.jp>
2022-08-14 14:22:59 +0900

    Update to 0.1.2

--- alticns.rb
+++ alticns.rb
@@ -1,8 +1,8 @@
 class Alticns < Formula
   desc "CLI to manage alternative icons for Mac apps easily"
   homepage ""
-  url "https://github.com/Fus1onDev/Alticns/archive/0.1.1.tar.gz"
-  sha256 "85e21929ab63ce20bacedf0463c23a61d416208fa28c2785e072e023bfaff98b"
+  url "https://github.com/Fus1onDev/Alticns/archive/0.1.2.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   def install
