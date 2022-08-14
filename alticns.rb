b47b6d31563c9712ef81ea54af749456076692ab
Fus1onDev <luke_s1220@yahoo.co.jp>
2022-08-14 14:24:37 +0900

    Fix sha256

--- alticns.rb
+++ alticns.rb
@@ -2,7 +2,7 @@ class Alticns < Formula
   desc "CLI to manage alternative icons for Mac apps easily"
   homepage ""
   url "https://github.com/Fus1onDev/Alticns/archive/0.1.2.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "442e7edadb7f44874f44a68a49fdcd95da51ceea77bfc49c2c95777ff70bf083"
   license "MIT"
 
   def install
