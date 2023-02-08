4c3fd655759c37ea0d5f707ef3976e8bf2fd3376
fus1ondev <luke_s1220@yahoo.co.jp>
2023-02-08 18:34:41 +0900

    Fix wrong tag name

--- genelic.rb
+++ genelic.rb
@@ -1,7 +1,7 @@
 class Genelic < Formula
   desc "⚖️ Generate a license file for your project easily"
   homepage "https://github.com/fus1ondev/genelic#readme"
-  url "https://github.com/fus1ondev/genelic/archive/refs/tags/1.0.0.tar.gz"
+  url "https://github.com/fus1ondev/genelic/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
