d514955a787a5ce3fe2992a14427c3f9a74e5003
fus1ondev <luke_s1220@yahoo.co.jp>
2023-03-06 19:34:13 +0900

    Update genelic to 1.2.0

--- genelic.rb
+++ genelic.rb
@@ -1,14 +1,14 @@
 class Genelic < Formula
   desc "⚖️ Generate a license file for your project easily"
   homepage "https://github.com/fus1ondev/genelic#readme"
-  url "https://github.com/fus1ondev/genelic/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "24ad7f5ad940a25910e813515eb95837f4f0c51e5d5660c4d51d1ef28b6b31ec"
+  url "https://github.com/fus1ondev/genelic/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "fb1d890372be36769ec92d861aecef9e5f923fa848187aac73f77ac68f999fa3"
   license "MIT"
 
   depends_on "deno" => :build
 
   def install
-    system "deno", "compile", "--allow-read", "--allow-write", "--allow-env", "--output=genelic", "mod.ts"
+    system "deno", "compile", "--allow-read", "--allow-write", "--allow-env", "--allow-sys", "--output=genelic", "mod.ts"
 
     bin.install "genelic"
   end
