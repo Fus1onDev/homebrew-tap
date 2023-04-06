7e59fcbd046b71d06f19a75281fc6300f6e521e3
fus1ondev <luke_s1220@yahoo.co.jp>
2023-04-06 13:20:54 +0900

    Update genelic to v1.3.0

--- genelic.rb
+++ genelic.rb
@@ -1,19 +1,19 @@
 class Genelic < Formula
   desc "⚖️ Generate a license file for your project easily"
   homepage "https://github.com/fus1ondev/genelic#readme"
-  url "https://github.com/fus1ondev/genelic/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "a8a5a3ed614c9f59efaa36bda96ea726aef021cbe416985936a4b056a1fb874e"
+  url "https://github.com/fus1ondev/genelic/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "ae692df1ec9d08bab5eef8ac7774ec692f4cff9e4a875c9aa1433eb1b8dfab0f"
   license "MIT"
 
   depends_on "deno" => :build
 
   def install
-    system "deno", "compile", "--allow-read", "--allow-write", "--allow-env", "--allow-sys", "--allow-net", "--allow-run", "--no-lock", "--output=genelic", "mod.ts"
+    system "deno", "compile", "-A", "--output=glc", "mod.ts"
 
-    bin.install "genelic"
+    bin.install "glc"
   end
 
   test do
-    assert_match "Title", shell_output("genelic list")
+    assert_match "Title", shell_output("glc list")
   end
 end
